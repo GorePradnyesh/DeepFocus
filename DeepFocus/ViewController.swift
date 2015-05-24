16b20eb3eb771dfe3f324ed11447ce9dc79f75e8
pgore <pgore@adobe.com>
2015-05-24 14:14:33 -0700

    Adding temp code to diplay a scaled image in the UIView

--- DeepFocus/ViewController.swift
+++ DeepFocus/ViewController.swift
@@ -12,14 +12,50 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.updateUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: private members
+    var imageView = UIImageView()
+
+    @IBOutlet weak var capture: UIButton!
+
+    @IBOutlet weak var imageViewContainer: UIView!{
+        didSet{
+            imageViewContainer.addSubview(imageView);
+        }
     }
+    
+    // MARK: private functions 
+    func updateUI(){
+        var urlString:NSString = "http://globe-views.com/dcim/dreams/car/car-03.jpg";
+        var URL = NSURL(string:urlString);
+        let qos = Int(QOS_CLASS_USER_INITIATED.value);
+        dispatch_async(dispatch_get_global_queue(qos, 0), { () -> Void in
+            if let imageData = NSData(contentsOfURL: URL!){
+                if let image = UIImage(data: imageData){
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.imageView.image = image;
+                        self.adjustHeight()
+                    });
+                }
+            }
+        });
+    }
+    
+    func adjustHeight(){
+        if(self.imageView.image?.aspectRatio > 0){
+            if let width = self.imageView.superview?.frame.size.width {
+                let height = width / self.imageView.image!.aspectRatio
+                self.imageView.frame = CGRect(x:0, y:0, width:width, height:height);
+            }
+        }
+    } // end of adjust width
+}
 
-
+extension UIImage{
+    var aspectRatio: CGFloat{
+        return ((size.height != 0) ? (size.width / size.height) : 0);
+    }
 }
 
