7114d32f2816ed17c9de639e69dd03d49a006d1d
pgore <pgore@adobe.com>
2015-05-27 22:28:05 -0700

    Move entry point to a code based ViewController

--- DeepFocus/AppDelegate.swift
+++ DeepFocus/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds);
+        let storyBoard = UIStoryboard(name: "EntryStoryBoard", bundle: nil);
+        let viewController = storyBoard.instantiateViewControllerWithIdentifier("ViewController") as UIViewController;
+        self.window?.rootViewController = viewController;
+        self.window?.makeKeyAndVisible();
         return true
     }
 
